b24bae7275ade80b096eecc4fe936270a7ef4007
林岳漫 <linyueman1@.jd.com>
2021-08-17 10:44:36 +0800

    更新1

--- helloGit/src/main/java/com/lym/git/helloGit.java
+++ helloGit/src/main/java/com/lym/git/helloGit.java
@@ -1,9 +1,11 @@
 package com.lym.git;
 
-
+import java.sql.SQLOutput;
 
 public class helloGit {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("更新1");
+
     }
 }
